303735f99d0cc36bd20ffac149f56b78b61711bf
Denys Radysh <radsdenys@gmail.com>
2022-11-22 23:56:57 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import axios from 'axios';
 
 function App() {
 
-const [data, setData] = useState({});
-const [location, setLocation] = useState();
+// const [data, setData] = useState({});
+// const [location, setLocation] = useState();
 
-const url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=bd791c17f5db20e1ea766717c1a0536c`;
-const searchLocation = (event) => {
-  if (event.key === 'Enter') {
-    axios.get(url).then((response) => {
-      setData(response.data)
-      console.log(response.data)
-    })
-    setLocation('');
-  }
-}
+// const url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=bd791c17f5db20e1ea766717c1a0536c`;
+// const searchLocation = (event) => {
+//   if (event.key === 'Enter') {
+//     axios.get(url).then((response) => {
+//       setData(response.data)
+//       console.log(response.data)
+//     })
+//     setLocation('');
+//   }
+//}
   return ( 
     <div className="App">
-      <input
+      {/* <input
             value={location}
             onChange={event => setLocation(event.target.value)}
             onKeyPress={searchLocation}
@@ -30,7 +30,7 @@ const searchLocation = (event) => {
     <div className='information'>
       <h2>{data.name}</h2>
       <h4>Temperature</h4>
-    </div>
+    </div> */}
 
 
     </div>
