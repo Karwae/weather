c7fd460ea9b3bedc7c2bbdb14a69d1d83737a6cc
Denys Radysh <radsdenys@gmail.com>
2023-03-31 22:30:34 +0300

    #1 connection weather api and get temp location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import axios from 'axios';
 
 function App() {
 
+  const inputRef = useRef(null);
+  const [apiData, setApiData] = useState(null);
+
+  const api_key = "df3886c4ae0b164958dcf4fd38b1575b";
+  
+  const gettingWeather = async () =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${api_key}`;
+    
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      setApiData(null);
+      console.log(data);
+      setApiData(data);
+  })
+  .catch((err) =>{
+    console.log(err);
+  });
+}
 
   return ( 
     <div className="App">
+      <input 
+      type = "text"
+      ref={inputRef}
+      placeholder = "Enter your location"
+      className='input-loc'
+      />
+      <button
+      onClick={gettingWeather}
+      >Click</button>
+
+    <div>
+    {apiData && (
+        <h2>{apiData?.name + ',' + apiData?.sys?.country}</h2>
+                )}
+   
+    <p>{apiData?.main?.temp}</p>
+
+    </div>
+
 
 
     </div>
