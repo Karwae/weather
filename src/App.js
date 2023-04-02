762aadfc2200e4ce3ab24d80b10d6d705377c3c4
Denys Radysh <radsdenys@gmail.com>
2023-04-02 22:01:47 +0300

    #2 Added weather for 5 days

--- src/App.js
+++ src/App.js
@@ -4,51 +4,55 @@ import axios from 'axios';
 
 function App() {
 
-  const inputRef = useRef(null);
-  const [apiData, setApiData] = useState(null);
+const inputRef = useRef(null);
+const [apiData, setApiData] = useState(null);
 
-  const api_key = "df3886c4ae0b164958dcf4fd38b1575b";
-  
-  const gettingWeather = async () =>{
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${api_key}`;
-    
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      setApiData(null);
-      console.log(data);
-      setApiData(data);
-  })
-  .catch((err) =>{
-    console.log(err);
-  });
+const api_key = "df3886c4ae0b164958dcf4fd38b1575b";
+
+const gettingWeather= async () =>{
+
+const url = `https://api.openweathermap.org/data/2.5/forecast/?q=${inputRef.current.value}&cnt=60&units=metric&appid=${api_key}`; 
+fetch(url)
+.then((res) => res.json())
+.then((data) => {
+  // setApiData(null);
+  console.log(data);
+  setApiData(data);
+})
+.catch((err) =>{
+console.log(err);
+});
+
 }
 
-  return ( 
-    <div className="App">
-      <input 
-      type = "text"
-      ref={inputRef}
-      placeholder = "Enter your location"
-      className='input-loc'
-      />
-      <button
-      onClick={gettingWeather}
-      >Click</button>
 
-    <div>
-    {apiData && (
-        <h2>{apiData?.name + ',' + apiData?.sys?.country}</h2>
-                )}
-   
-    <p>{apiData?.main?.temp}</p>
-
-    </div>
-
-
-
-    </div>
-  );
+return ( 
+  <div className="App">
+    <input 
+    type = "text"
+    ref={inputRef}
+    placeholder = "Enter your location"
+    className='input-loc'
+    />
+    <button
+    onClick={gettingWeather}
+    >Click</button>
+<div>
+{apiData?.list?.map((item, index) => {
+  if (index % 8 === 0) {
+    return (
+      <div key={index}>
+        {item?.dt_txt}
+        <h2>{item?.main.temp}</h2>
+      </div>
+    );
+  } else {
+    return null;
   }
+})}
+</div>
+  </div>
+);
+}
 
 export default App;
